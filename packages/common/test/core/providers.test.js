bb2af082b807534ed4617deeba10ffe126c6c9f1
rajitha1998 <rajitha1998@gmail.com>
2020-08-26 19:40:55 +0530

    Fixing config file import error

--- packages/common/test/core/providers.test.js
+++ packages/common/test/core/providers.test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const assert = chai.assert;
-const providers = require("../core/providers");
+const providers = require("../../core/providers");
 
 describe("Providers list", () => {
   it("should check for providers", done => {
