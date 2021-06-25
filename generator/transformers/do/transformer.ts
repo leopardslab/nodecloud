aebff82e7e352fdcb7ede375cec315d1f044a32c
mbcse <mbcse50@gmail.com>
2021-06-25 21:02:10 +0530

    Added  transformation function for  adding Indentifiers/Parameters

--- generator/transformers/do/transformer.ts
+++ generator/transformers/do/transformer.ts
@@ -53,5 +53,83 @@ export async function transform(code: ts.SourceFile, classData: any): Promise<st
     return ts.visitNode(rootNode, visit);
   };
 
+/*
+* Add Indentifier Transformation function
+*/
+  const addIdentifiers = <T extends ts.Node>(context: ts.TransformationContext) => (rootNode: T) => {
+    let count = 0;
+    function visit(node: ts.Node): ts.Node {
+      if (ts.isMethodDeclaration(node)) {
+        const parameters = classData.functions[count].params.map(param => {
+          const paramNode = ts.createParameter(
+            undefined,
+            undefined,
+            undefined,
+            param.name
+          );
 
+          if (param.optional) {
+            paramNode.initializer = ts.createIdentifier("undefined");
+          }
+
+          return paramNode;
+        });
+
+        node.parameters = parameters;
+      }
+
+      if (ts.isIdentifier(node) && dummyIdentifiers.includes(node.text)) {
+        let updatedIdentifier;
+
+        switch (node.text) {
+          case "ClassName":
+            updatedIdentifier = ts.updateIdentifier(
+              ts.createIdentifier("DO_" + classData.serviceName)
+            );
+            break;
+          case "_sdkClassName":
+            updatedIdentifier = ts.updateIdentifier(
+              ts.createIdentifier(
+                "_" +
+                  classData.className.charAt(0).toLowerCase() +
+                  classData.className.substr(1)
+              )
+            );
+            break;
+          case "SDKClassName":
+            updatedIdentifier = ts.updateIdentifier(
+              ts.createIdentifier(classData.className.charAt(0).toLowerCase()+
+              classData.className.substr(1))
+            );
+            break;
+          case "SDKFunctionName":
+            updatedIdentifier = ts.updateIdentifier(
+              ts.createIdentifier(classData.functions[count].SDKFunctionName)
+            );
+            count++;
+        }
+
+        return updatedIdentifier;
+      }
+
+      if (ts.isCallExpression(node)) {
+        node.expression.forEachChild(childNode => {
+          if (
+            ts.isIdentifier(childNode) &&
+            childNode.text === "SDKFunctionName"
+          ) {
+            const args = classData.functions[count].params.map(param =>
+              ts.createIdentifier(param.name)
+            );
+            node.arguments = args.concat(node.arguments);
+          }
+        });
+      }
+
+      return ts.visitEachChild(node, visit, context);
+    }
+    return ts.visitNode(rootNode, visit);
+  };
+
+ 
 }
