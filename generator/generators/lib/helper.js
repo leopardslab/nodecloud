a982204947d25778ec4270def8dcb0e9ede17402
Rajitha Warusavitarana <rajitha1998@gmail.com>
2021-08-19 19:08:19 +0530

    Final touches for do-plugin and code formattings

--- generator/generators/lib/helper.js
+++ generator/generators/lib/helper.js
@@ -9,40 +9,39 @@ var awsHelper_2 = require("../lib/aws/awsHelper");
 var gcpHelper_2 = require("../lib/googleCloud/gcpHelper");
 var azureHelper_2 = require("../lib/azure/azureHelper");
 var dirMap = {
-    appServices: ["PaaS"],
-    compute: ["ComputeInstance", "Kubernetes", "Container"],
-    database: ["NoSqlIndexed", "RDBMS", "NoSql"],
-    management: ["Monitoring", "KeyManagement", "NotificationService"],
-    network: ["DNS", "LoadBalancer"],
-    security: ["IAM"],
-    storage: ["StorageBucket", "BlockStorage", "ArchivalStorage"],
-    artificialinteligence: ["Translation"]
+  appServices: ["PaaS"],
+  compute: ["ComputeInstance", "Kubernetes", "Container"],
+  database: ["NoSqlIndexed", "RDBMS", "NoSql"],
+  management: ["Monitoring", "KeyManagement", "NotificationService"],
+  network: ["DNS", "LoadBalancer"],
+  security: ["IAM"],
+  storage: ["StorageBucket", "BlockStorage", "ArchivalStorage"],
+  artificialinteligence: ["Translation"]
 };
 function printFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err)
-            throw err;
-    });
+  fs.writeFile(fileName, data, function(err) {
+    if (err) throw err;
+  });
 }
 exports.printFile = printFile;
 var groupers = {
-    aws: awsHelper_1.groupAWSMethods,
-    gcp: gcpHelper_1.groupGCPMethods,
-    azure: azureHelper_1.groupAzureMethods
+  aws: awsHelper_1.groupAWSMethods,
+  gcp: gcpHelper_1.groupGCPMethods,
+  azure: azureHelper_1.groupAzureMethods
 };
 exports.groupers = groupers;
 var filters = {
-    aws: awsHelper_2.filterAWSMethods,
-    gcp: gcpHelper_2.filterGCPMethods,
-    azure: azureHelper_2.filterAzureMethods
+  aws: awsHelper_2.filterAWSMethods,
+  gcp: gcpHelper_2.filterGCPMethods,
+  azure: azureHelper_2.filterAzureMethods
 };
 exports.filters = filters;
-var getDir = function (service) {
-    for (var dir in dirMap) {
-        if (dirMap[dir].includes(service)) {
-            return dir;
-        }
+var getDir = function(service) {
+  for (var dir in dirMap) {
+    if (dirMap[dir].includes(service)) {
+      return dir;
     }
-    throw new Error("Not a valid service: " + service);
+  }
+  throw new Error("Not a valid service: " + service);
 };
 exports.getDir = getDir;
