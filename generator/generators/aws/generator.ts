d826b392ded6989a1cbedc3e150a541b9e658816
Pranjal Walia <pranjalwalia77@gmail.com>
2022-07-06 16:33:36 +0530

    chore: update formatter conf and apply formatter

--- generator/generators/aws/generator.ts
+++ generator/generators/aws/generator.ts
@@ -1,125 +1,133 @@
-import * as fs from "fs";
-import { createSourceFile, ScriptTarget, SyntaxKind } from "typescript";
+import * as fs from 'fs';
+import { createSourceFile, ScriptTarget, SyntaxKind } from 'typescript';
 
-import { getAST } from "../../parsers/aws/parser";
-import { transform } from "../../transformers/aws/transformer";
-import { filters, getDir,groupers, printFile } from "../lib/helper";
+import { getAST } from '../../parsers/aws/parser';
+import { transform } from '../../transformers/aws/transformer';
+import { filters, getDir, groupers, printFile } from '../lib/helper';
 
 interface FunctionData {
-  functionName: string;
-  SDKFunctionName: string;
-  params: param[];
+	functionName: string;
+	SDKFunctionName: string;
+	params: param[];
 }
 
 interface param {
-  name: string;
-  type: string;
-  typeName: string;
+	name: string;
+	type: string;
+	typeName: string;
 }
 
 interface ClassData {
-  className: string;
-  functions: FunctionData[];
-  serviceName: string;
+	className: string;
+	functions: FunctionData[];
+	serviceName: string;
 }
 
-const dummyFile = process.cwd() + "/dummyClasses/aws.js";
+const dummyFile = process.cwd() + '/dummyClasses/aws.js';
 const dummyAst = createSourceFile(
-  dummyFile,
-  fs.readFileSync(dummyFile).toString(),
-  ScriptTarget.Latest,
-  true
+	dummyFile,
+	fs.readFileSync(dummyFile).toString(),
+	ScriptTarget.Latest,
+	true
 );
 
 export function extractSDKData(sdkClassAst, serviceClass) {
-  let methods: FunctionData[] = [];
-  const functions = [];
+	let methods: FunctionData[] = [];
+	const functions = [];
 
-  Object.keys(serviceClass).map((key, index) => {
-    functions.push(serviceClass[key].split(" ")[1]);
-  });
+	Object.keys(serviceClass).map((key, index) => {
+		functions.push(serviceClass[key].split(' ')[1]);
+	});
 
-  sdkClassAst.members.map(method => {
-    if (method.name && functions.includes(method.name.text)) {
-      let name;
-      Object.keys(serviceClass).map((key, index) => {
-        if (serviceClass[key].split(" ")[1] === method.name.text) {
-          name = key;
-        }
-      });
+	sdkClassAst.members.map(method => {
+		if (method.name && functions.includes(method.name.text)) {
+			let name;
+			Object.keys(serviceClass).map((key, index) => {
+				if (serviceClass[key].split(' ')[1] === method.name.text) {
+					name = key;
+				}
+			});
 
-      const parameters = [];
-      method.parameters.map(param => {
-        if (param.name.text !== "callback") {
-          const parameter = {
-            name: param.name.text,
-            optional: param.questionToken ? true : false,
-            type: SyntaxKind[param.type.kind],
-            typeName: null
-          };
+			const parameters = [];
+			method.parameters.map(param => {
+				if (param.name.text !== 'callback') {
+					const parameter = {
+						name: param.name.text,
+						optional: param.questionToken ? true : false,
+						type: SyntaxKind[param.type.kind],
+						typeName: null,
+					};
 
-          if (parameter.type === "TypeReference" && param.type.typeName) {
-            parameter.typeName = param.type.typeName.right.text;
-          }
+					if (
+						parameter.type === 'TypeReference' &&
+						param.type.typeName
+					) {
+						parameter.typeName = param.type.typeName.right.text;
+					}
 
-          parameters.push(parameter);
-        }
-      });
+					parameters.push(parameter);
+				}
+			});
 
-      methods.push({
-        functionName: name.toString(),
-        SDKFunctionName: method.name.text.toString(),
-        params: parameters
-      });
-    }
-  });
+			methods.push({
+				functionName: name.toString(),
+				SDKFunctionName: method.name.text.toString(),
+				params: parameters,
+			});
+		}
+	});
 
-  const groupedMethods = groupers.aws(methods);
-  methods = filters.aws(groupedMethods);
+	const groupedMethods = groupers.aws(methods);
+	methods = filters.aws(groupedMethods);
 
-  const classData: ClassData = {
-    className: sdkClassAst.name.text,
-    functions: methods,
-    serviceName: null
-  };
+	const classData: ClassData = {
+		className: sdkClassAst.name.text,
+		functions: methods,
+		serviceName: null,
+	};
 
-  return classData;
+	return classData;
 }
 
 export function generateAWSClass(serviceClass, serviceName) {
-  const sdkFile = serviceClass[Object.keys(serviceClass)[0]].split(" ")[0];
-  getAST(sdkFile).then(async result => {
-    const sdkClassAst = result;
-    try {
-      const classData: ClassData = extractSDKData(sdkClassAst, serviceClass);
-      classData.serviceName = serviceName;
-      const output = await transform(dummyAst, classData);
-      let filePath;
-      const dir = getDir(serviceName);
-      if (!fs.existsSync(process.cwd() + "/generatedClasses/AWS/" + dir)) {
-        fs.mkdirSync(process.cwd() + "/generatedClasses/AWS/" + dir);
-      }
-      if (/^[A-Z]*$/.test(serviceName)) {
-        filePath =
-          process.cwd() +
-          "/generatedClasses/AWS/" +
-          dir +
-          "/aws-" +
-          serviceName +
-          ".js";
-      } else {
-        filePath =
-          process.cwd() +
-          "/generatedClasses/AWS/" +
-          dir +
-          "/aws-" +
-          serviceName.charAt(0).toLowerCase() +
-          serviceName.slice(1) +
-          ".js";
-      }
-      printFile(filePath, output);
-    } catch (e) {
-      console.error(e);
-    }
-  });
+	const sdkFile = serviceClass[Object.keys(serviceClass)[0]].split(' ')[0];
+	getAST(sdkFile).then(async result => {
+		const sdkClassAst = result;
+		try {
+			const classData: ClassData = extractSDKData(
+				sdkClassAst,
+				serviceClass
+			);
+			classData.serviceName = serviceName;
+			const output = await transform(dummyAst, classData);
+			let filePath;
+			const dir = getDir(serviceName);
+			if (
+				!fs.existsSync(process.cwd() + '/generatedClasses/AWS/' + dir)
+			) {
+				fs.mkdirSync(process.cwd() + '/generatedClasses/AWS/' + dir);
+			}
+			if (/^[A-Z]*$/.test(serviceName)) {
+				filePath =
+					process.cwd() +
+					'/generatedClasses/AWS/' +
+					dir +
+					'/aws-' +
+					serviceName +
+					'.js';
+			} else {
+				filePath =
+					process.cwd() +
+					'/generatedClasses/AWS/' +
+					dir +
+					'/aws-' +
+					serviceName.charAt(0).toLowerCase() +
+					serviceName.slice(1) +
+					'.js';
+			}
+			printFile(filePath, output);
+		} catch (e) {
+			console.error(e);
+		}
+	});
 }
--- generator/parsers/aws/parser.ts
+++ generator/parsers/aws/parser.ts
@@ -1,39 +1,40 @@
-import * as fs from "fs";
-import * as path from "path";
-import { createSourceFile, ScriptTarget, SyntaxKind } from "typescript";
+import * as fs from 'fs';
+import * as path from 'path';
+import { createSourceFile, ScriptTarget, SyntaxKind } from 'typescript';
 
 export function getAST(sdkFileName) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const file = path.join(
-        __dirname,
-        "../../../node_modules/aws-sdk/clients/" + sdkFileName.toLowerCase()
-      );
-      const ast = createSourceFile(
-        file,
-        fs.readFileSync(file).toString(),
-        ScriptTarget.Latest,
-        true
-      );
+	return new Promise(async (resolve, reject) => {
+		try {
+			const file = path.join(
+				__dirname,
+				'../../../node_modules/aws-sdk/clients/' +
+					sdkFileName.toLowerCase()
+			);
+			const ast = createSourceFile(
+				file,
+				fs.readFileSync(file).toString(),
+				ScriptTarget.Latest,
+				true
+			);
 
-      let cloned = null;
-      await ast.forEachChild(child => {
-        if (SyntaxKind[child.kind] === "ClassDeclaration") {
-          cloned = Object.assign({}, child);
-        }
-      });
+			let cloned = null;
+			await ast.forEachChild(child => {
+				if (SyntaxKind[child.kind] === 'ClassDeclaration') {
+					cloned = Object.assign({}, child);
+				}
+			});
 
-      if (!cloned) {
-        reject(new Error("Class not found!"));
-      } else {
-        resolve(cloned);
-      }
-    } catch (error) {
-      if (error.code === "ENOENT") {
-        reject(new Error("File not found!"));
-      } else {
-        reject(error);
-      }
-    }
-  });
+			if (!cloned) {
+				reject(new Error('Class not found!'));
+			} else {
+				resolve(cloned);
+			}
+		} catch (error) {
+			if (error.code === 'ENOENT') {
+				reject(new Error('File not found!'));
+			} else {
+				reject(error);
+			}
+		}
+	});
 }
