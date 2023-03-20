48d252d0b3ad8e00d8272a5e1d1ce05197ed07ce
Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>
2023-03-20 19:52:09 +0530

    Initialised Linode
    Signed-off-by: Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>

---
+++ generator/dummyClasses/linode.js
@@ -0,0 +1,24 @@
+class ClassName {
+    /**
+     *
+     * @param {module} do Linode SDK
+     * @param {object} options SDK options
+     */
+    constructor(linodeSdk, linodeToken) {
+     this._linode = linodeSdk;
+     this._linodeToken = linodeToken;
+    }
+  
+    function() {
+    this._linode.setToken(this._linodeToken)
+      return new Promise((resolve, reject) => {
+        this._linode
+          .SDKFunctionName()
+          .then(data => resolve(data))
+          .catch(err => reject(err));
+      });
+    }
+  }
+  
+  module.exports = ClassName;
+  
