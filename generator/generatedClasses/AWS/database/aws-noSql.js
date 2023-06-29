bdcb130005c01973ac876ec837273b77010e1217
Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>
2023-06-29 21:48:03 +0530

    Added Oracle extractor
    Signed-off-by: Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>

--- generator/generatedClasses/AWS/database/aws-noSql.js
+++ generator/generatedClasses/AWS/database/aws-noSql.js
@@ -4,119 +4,113 @@
  * @category AWS
  */
 class AWS_NoSql {
-    /**
-     *
-     * @param {module} aws AWS SDK
-     * @param {object} options SDK options
-     */
-    constructor(aws, options) {
-        this._AWS = aws;
-        this._apiVersion = options.apiVersion;
-        this._dynamoDB = new this._AWS.DynamoDB({
-            apiVersion: this._apiVersion
-        });
-    }
-    /**
-     * Trigers the createTable function of DynamoDB
-     * @param {CreateTableInput} params - Data required for createTable
-     * @returns {Promise<createTableResponse>}
-     */
-    createTable(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.createTable(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-    /**
-     * Trigers the deleteItem function of DynamoDB
-     * @param {DeleteItemInput} params - Data required for deleteItem
-     * @returns {Promise<deleteItemResponse>}
-     */
-    deleteItem(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.deleteItem(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-    /**
-     * Trigers the deleteTable function of DynamoDB
-     * @param {DeleteTableInput} params - Data required for deleteTable
-     * @returns {Promise<deleteTableResponse>}
-     */
-    deleteTable(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.deleteTable(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-    /**
-     * Trigers the putItem function of DynamoDB
-     * @param {PutItemInput} params - Data required for putItem
-     * @returns {Promise<putItemResponse>}
-     */
-    createItem(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.putItem(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-    /**
-     * Trigers the query function of DynamoDB
-     * @param {QueryInput} params - Data required for query
-     * @returns {Promise<queryResponse>}
-     */
-    query(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.query(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-    /**
-     * Trigers the updateItem function of DynamoDB
-     * @param {UpdateItemInput} params - Data required for updateItem
-     * @returns {Promise<updateItemResponse>}
-     */
-    updateItem(params) {
-        return new Promise((resolve, reject) => {
-            this._dynamoDB.updateItem(params, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
+	/**
+	 *
+	 * @param {module} aws AWS SDK
+	 * @param {object} options SDK options
+	 */
+	constructor(aws, options) {
+		this._AWS = aws;
+		this._apiVersion = options.apiVersion;
+		this._dynamoDB = new this._AWS.DynamoDB({
+			apiVersion: this._apiVersion,
+		});
+	}
+	/**
+	 * Trigers the createTable function of DynamoDB
+	 * @param {CreateTableInput} params - Data required for createTable
+	 * @returns {Promise<createTableResponse>}
+	 */
+	createTable(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.createTable(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+	/**
+	 * Trigers the deleteItem function of DynamoDB
+	 * @param {DeleteItemInput} params - Data required for deleteItem
+	 * @returns {Promise<deleteItemResponse>}
+	 */
+	deleteItem(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.deleteItem(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+	/**
+	 * Trigers the deleteTable function of DynamoDB
+	 * @param {DeleteTableInput} params - Data required for deleteTable
+	 * @returns {Promise<deleteTableResponse>}
+	 */
+	deleteTable(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.deleteTable(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+	/**
+	 * Trigers the putItem function of DynamoDB
+	 * @param {PutItemInput} params - Data required for putItem
+	 * @returns {Promise<putItemResponse>}
+	 */
+	createItem(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.putItem(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+	/**
+	 * Trigers the query function of DynamoDB
+	 * @param {QueryInput} params - Data required for query
+	 * @returns {Promise<queryResponse>}
+	 */
+	query(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.query(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+	/**
+	 * Trigers the updateItem function of DynamoDB
+	 * @param {UpdateItemInput} params - Data required for updateItem
+	 * @returns {Promise<updateItemResponse>}
+	 */
+	updateItem(params) {
+		return new Promise((resolve, reject) => {
+			this._dynamoDB.updateItem(params, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
 }
 module.exports = AWS_NoSql;
--- generator/generatedClasses/Azure/compute/azure-computeInstance.js
+++ generator/generatedClasses/Azure/compute/azure-computeInstance.js
@@ -1,148 +1,207 @@
-const { ComputeManagementClient } = require("@azure/arm-compute");
+const { ComputeManagementClient } = require('@azure/arm-compute');
 /*This is an auto generated class, please do not change.*/
 /**
  * Class to create a ComputeInstance object
  * @category Azure
  */
 class Azure_ComputeInstance {
-    /**
-     *
-     * @param {module} azureRestSdk Azure Rest SDK
-     */
-    constructor(azureRestSdk) {
-        this._azureRestSdk = azureRestSdk;
-    }
-    /**
-     * Trigers the createOrUpdate function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {StringKeyword} vmName - Mandatory parameter
-     * @param {TypeReference} parameters - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<createOrUpdateResponse>}
-     */
-    create(resourceGroupName, vmName, parameters, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.createOrUpdate(resourceGroupName, vmName, parameters, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
-    /**
-     * Trigers the list function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<listResponse>}
-     */
-    list(resourceGroupName, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.list(resourceGroupName, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
-    /**
-     * Trigers the start function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {StringKeyword} vmName - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<startResponse>}
-     */
-    start(resourceGroupName, vmName, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.start(resourceGroupName, vmName, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
-    /**
-     * Trigers the powerOff function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {StringKeyword} vmName - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<powerOffResponse>}
-     */
-    stop(resourceGroupName, vmName, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.powerOff(resourceGroupName, vmName, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
-    /**
-     * Trigers the restart function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {StringKeyword} vmName - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<restartResponse>}
-     */
-    reboot(resourceGroupName, vmName, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.restart(resourceGroupName, vmName, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
-    /**
-     * Trigers the deleteMethod function of compute
-     * @param {StringKeyword} resourceGroupName - Mandatory parameter
-     * @param {StringKeyword} vmName - Mandatory parameter
-     * @param {TypeReference} [options] - Optional parameter
-     * @returns {Promise<deleteMethodResponse>}
-     */
-    destroy(resourceGroupName, vmName, options = undefined) {
-        return new Promise((resolve, reject) => {
-            this._azureRestSdk
-                .loginWithServicePrincipalSecretWithAuthResponse(process.env.AZURE_CLIENT_ID, process.env.AZURE_CLIENT_SECRET, process.env.AZURE_TENANT_ID)
-                .then(authres => {
-                const client = new ComputeManagementClient(authres.credentials, process.env.AZURE_SUBSCRIPTION_ID);
-                client.virtualMachines.deleteMethod(resourceGroupName, vmName, options).then(result => {
-                    resolve(result);
-                });
-            })
-                .catch(err => {
-                reject(err);
-            });
-        });
-    }
+	/**
+	 *
+	 * @param {module} azureRestSdk Azure Rest SDK
+	 */
+	constructor(azureRestSdk) {
+		this._azureRestSdk = azureRestSdk;
+	}
+	/**
+	 * Trigers the createOrUpdate function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {StringKeyword} vmName - Mandatory parameter
+	 * @param {TypeReference} parameters - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<createOrUpdateResponse>}
+	 */
+	create(resourceGroupName, vmName, parameters, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.createOrUpdate(
+							resourceGroupName,
+							vmName,
+							parameters,
+							options
+						)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+	/**
+	 * Trigers the list function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<listResponse>}
+	 */
+	list(resourceGroupName, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.list(resourceGroupName, options)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+	/**
+	 * Trigers the start function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {StringKeyword} vmName - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<startResponse>}
+	 */
+	start(resourceGroupName, vmName, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.start(resourceGroupName, vmName, options)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+	/**
+	 * Trigers the powerOff function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {StringKeyword} vmName - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<powerOffResponse>}
+	 */
+	stop(resourceGroupName, vmName, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.powerOff(resourceGroupName, vmName, options)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+	/**
+	 * Trigers the restart function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {StringKeyword} vmName - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<restartResponse>}
+	 */
+	reboot(resourceGroupName, vmName, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.restart(resourceGroupName, vmName, options)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+	/**
+	 * Trigers the deleteMethod function of compute
+	 * @param {StringKeyword} resourceGroupName - Mandatory parameter
+	 * @param {StringKeyword} vmName - Mandatory parameter
+	 * @param {TypeReference} [options] - Optional parameter
+	 * @returns {Promise<deleteMethodResponse>}
+	 */
+	destroy(resourceGroupName, vmName, options = undefined) {
+		return new Promise((resolve, reject) => {
+			this._azureRestSdk
+				.loginWithServicePrincipalSecretWithAuthResponse(
+					process.env.AZURE_CLIENT_ID,
+					process.env.AZURE_CLIENT_SECRET,
+					process.env.AZURE_TENANT_ID
+				)
+				.then(authres => {
+					const client = new ComputeManagementClient(
+						authres.credentials,
+						process.env.AZURE_SUBSCRIPTION_ID
+					);
+					client.virtualMachines
+						.deleteMethod(resourceGroupName, vmName, options)
+						.then(result => {
+							resolve(result);
+						});
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
 }
 module.exports = Azure_ComputeInstance;
